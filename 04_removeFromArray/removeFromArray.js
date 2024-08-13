8c8ed8c0b079673657f2e400e613aa178d7bb4c8
Ben Sterne <31416147+bensterne@users.noreply.github.com>
2024-08-13 13:52:17 -0700

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-};
+    newArray =[...arguments[0]];
+    for(let i=1;i<arguments.length;i++){
+                newArray = newArray.filter((value) => value !== arguments[i]);    
+    }
+return newArray;
+}
+
+console.log(removeFromArray([1,2,3],2));
+console.log(removeFromArray([1,2,3],2,3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
