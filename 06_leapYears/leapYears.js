58f5a07d52ec74c6ce94fea3d5ca6b20776f2da4
Ben Sterne <31416147+bensterne@users.noreply.github.com>
2024-08-13 14:27:39 -0700

    complete exercise 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,4 @@
 const leapYears = function(year) {
-    /*Leap years are years divisible by four (like 1984 and 2004).
-     However, years divisible by 100 are not leap years (such as 1800 and 1900) 
-     unless they are divisible by 400 (like 1600 and 2000, which were in 
-     fact leap years). (Yes, it's all pretty confusing)*/
      if (year % 400 == 0){
         return true;
      } else {
@@ -17,6 +13,11 @@ const leapYears = function(year) {
         }
      }
 };
+
+/*Leap years are years divisible by four (like 1984 and 2004).
+ However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+ unless they are divisible by 400 (like 1600 and 2000, which were in 
+ fact leap years). (Yes, it's all pretty confusing)*/
 
 console.log(leapYears(4));
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let f = (+temp-32) * (5/9);
+  f = Math.round(f*10)/10;
+  return f;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let c = (+temp * (9/5))+32;
+  c = Math.round(c*10)/10;
+  return c;
 };
+
+//console.log(convertToCelsius(40));
+//console.log(convertToFahrenheit(100));
 
 // Do not edit below this line
 module.exports = {
