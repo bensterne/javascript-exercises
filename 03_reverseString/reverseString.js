29332ce5ec50ee68a5ea45a17c2766a8d7679486
Ben Sterne <31416147+bensterne@users.noreply.github.com>
2024-08-13 12:25:17 -0700

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let strArray = string.split('');
+    let output ='';
+    for (const letter in strArray){
+        output = strArray[letter] + output;
+    }
+return output;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
