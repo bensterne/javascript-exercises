55a40cf9c16f2fdcd2b927cb2a116d08ab78e444
Ben Sterne <31416147+bensterne@users.noreply.github.com>
2024-08-23 09:45:06 -0700

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    let lastValue=0;
+    let currentValue =1;
+    let value = 0;
+    memberInt = parseInt(member);
+
+    if (typeof member !== 'number') {
+        memberInt = parseInt(memberInt)
+    } else {
+        memberInt = member
+    }
+    if (memberInt < 0){
+        value = "OOPS";
+    } else if (memberInt == 1){
+        value = 1;
+    } else {
+    for (let i=1;i<memberInt;i++){
+        value = lastValue + currentValue;
+        lastValue = currentValue;
+        currentValue = value;
+    }
+}
+    return value;
 
 };
+
+// for 1
+
 
 // Do not edit below this line
 module.exports = fibonacci;
