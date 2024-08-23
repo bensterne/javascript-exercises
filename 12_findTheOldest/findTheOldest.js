188deb954c5eb0c2c7523882ebef2a817de76187
Ben Sterne <31416147+bensterne@users.noreply.github.com>
2024-08-23 10:03:54 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.sort(function(a,b){
+    const lastAge = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+    const thisAge = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+    return thisAge- lastAge;
+    })[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
