52102a3c5b2c10e2d0eac6c95b1979e7fe123a67
Ben Sterne <31416147+bensterne@users.noreply.github.com>
2024-08-22 18:02:33 -0700

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
 
+const palindromes = function (string) {
+
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    string = string.split(" ").join("");
+    let ifPalindrome = (string.toLowerCase() === string.reverse().toLowerCase());
+    return ifPalindrome;
+
+    // if (string.length % 2) {
+    //     ifPalindrome = string.substr(1,(string.length/2)) === reverseString(string.substr(-1,(string.length/2)));
+    // } else {
+    //     ifPalindrome = string.substr(1,((string.length-1)/2)) === reverseString(string.substr(-1,((string.length-1)/2)));
+
+    // }
+    // console.log(string.substr(1,(string.length/2)));
 };
 
 // Do not edit below this line
