5cb289369c43da68877101a4faeb2db37f929bb6
Ben Sterne <31416147+bensterne@users.noreply.github.com>
2024-08-22 17:33:34 -0700

    complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,23 @@
-const add = function() {
-	
-};
+const add = (a,b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a,b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (a) => a.reduce((total, current) => total + current, 0);
 
-const multiply = function() {
+const multiply = (a) => a.reduce((total, current) => total * current, 1);
 
-};
+const power = (a,b) => a ** b;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  output = 1;
+  if (a>=1){
+    for (let i=1;i<=a;i++){
+      output *= i;
+    }
+  return output;
+} else if (a == 0){
+  return 1;
+}
 };
 
 // Do not edit below this line
